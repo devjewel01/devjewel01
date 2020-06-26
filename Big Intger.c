c8819d04b599adbb4be6822c3b972927b14d623a
DevJewel <58494211+DevJewel143@users.noreply.github.com>
2020-06-26 12:45:54 +0600

    Add files via upload
    big integer

---
+++ Big Intger.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+int mult(int size,int res[],int x)
+    {
+    int carry=0,prod;
+    for(int j=0;j<size;j++)
+        {
+        prod=res[j]*x+carry;
+        res[j]=prod%10;
+        carry=prod/10;
+    }
+    while(carry)
+        {
+        res[size]=carry%10;
+        carry=carry/10;
+        size++;
+    }
+    return size;
+}
+void fact(int n)
+    {
+    int i,size;
+    int res[200];
+    res[0]=1;
+    size=1;
+    for(i=2;i<=n;i++)
+        {
+        size=mult(size,res,i);
+    }
+    for(i=size-1;i>=0;i--)
+        printf("%d",res[i]);
+
+}
+int main() {
+int n;
+    scanf("%d",&n);
+    fact(n);
+    return 0;
+}
